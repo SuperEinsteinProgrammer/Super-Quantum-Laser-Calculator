3b4b06f33de6c6440c81e82ac452aa68005ebb0f
SuperEinsteinProgrammer <bigbellygigglermason@yahoo.com>
2026-01-08 14:14:25 +0200

    clac
    This is the calculator.

---
+++ calc.py
@@ -0,0 +1,24 @@
+import time
+
+def sum(a, b):
+    return (a + b)
+print("Advanced scientific quantum calc (calculator)")
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print("Calculating...")
+time.sleep(2)
+
+print("Doing math...")
+time.sleep(2)
+
+print("Solving equations...")
+time.sleep(2)
+
+if a == (6) and b == (7):
+    print("67 boiii")
+    exit()
+if a + b > 10:
+    print("i cant count big numbers bro")
+else: 
+    print(sum(a, b))
